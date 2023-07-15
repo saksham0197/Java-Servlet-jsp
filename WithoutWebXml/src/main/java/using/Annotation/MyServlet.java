179283f482477c2041d68825c332ae06011e7412
saksham0197 <saksham0197@gmail.com>
2023-07-15 10:10:14 +0530

    Without XML

---
+++ WithoutWebXml/src/main/java/using/Annotation/MyServlet.java
@@ -0,0 +1,20 @@
+package using.Annotation;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/Mylink")
+public class MyServlet extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("Using Annotation & without using Web.Xml");
+	}
+	
+
+}
