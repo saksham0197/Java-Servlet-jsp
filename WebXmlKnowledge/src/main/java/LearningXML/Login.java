1241294a4b61e08c2366cded52ae4d99aaf35cc0
saksham0197 <saksham0197@gmail.com>
2023-07-15 10:18:08 +0530

    WITH WEB.XML

---
+++ WebXmlKnowledge/src/main/java/LearningXML/Login.java
@@ -0,0 +1,19 @@
+package LearningXML;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Login extends HttpServlet {
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("I am in Service Method");
+		
+	}
+	
+
+}
